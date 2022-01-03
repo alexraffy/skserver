bd6debdd7f45c9678b842f43fe01289c5b6e1503
alexraffy <92450474+alexraffy@users.noreply.github.com>
2022-01-03 16:28:36 +0100

    fix perf_hooks

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,22 @@ import {DBData} from "sksql";
 import fs from "fs";
 import {CSocket} from "./Socket/CSocket";
 
+const {
+    performance,
+    PerformanceObserver
+} = require('perf_hooks');
+
+
+
 const VERSION = 1.0;
 
 export async function main() {
     const databasePath = process.env.SKDB_PATH;
     const workerId = process.env.SKWORKER_ID;
     const sport = process.env.SKWORKER_PORT;
+    const sAlive = process.env.SKWORKER_ALIVE;
     const port = parseInt(sport);
-    const alive = 60 * 5;
+    const alive = parseInt(sAlive);
     await checkFolders(databasePath);
     let _ = new Logger(databasePath, workerId);
     Logger.instance.write("SKServer v" + VERSION);
