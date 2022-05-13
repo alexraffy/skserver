68e173832f280598f0de84f2ae2a23e53c4d3bcf
alexraffy <92450474+alexraffy@users.noreply.github.com>
2022-05-13 16:40:32 +0200

    7z

--- src/Backup/backup.ts
+++ src/Backup/backup.ts
@@ -8,8 +8,12 @@ export function backup(folder: string, db: SKSQL, callback: (db: SKSQL, backupNa
     let backup_name = new Date().toISOString();
     Logger.instance.write("Backup in progress: " + backup_name);
     let zipFile = path.normalize(`${folder}/backups/${backup_name}.zip`);
-    let source = path.normalize(`${folder}/db`);
-    let command = `zip -r ${zipFile} ${source}`;
+    let source = path.normalize(`${folder}/db/*`);
+    //let command = `zip -r ${zipFile} ${source}`;
+    let command = `7z a ${zipFile} ${source}`;
+
+
+
     exec_cmd(command).then((content) => {
         if (content.error !== undefined && content.error !== "") {
             Logger.instance.write("ERROR: ");
